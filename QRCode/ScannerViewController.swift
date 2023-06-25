8895344e33e55eeba8b76621e2ed8c0b6f5b2975
Helia <helia.f76@gmail.com>
2023-06-25 10:46:05 +0330

    init: commit

--- QRCode/ScannerViewController.swift
+++ QRCode/ScannerViewController.swift
@@ -5,4 +5,44 @@
 //  Created by Hell Yeah on 2023-06-24.
 //
 
-import Foundation
+import AVFoundation
+import UIKit
+
+class ScannerViewController: UIViewController {
+
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    weak var delegate: AVCaptureMetadataOutputObjectsDelegate?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let captureDevice = AVCaptureDevice.default(for: .video)
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice!)
+            captureSession = AVCaptureSession()
+            captureSession.addInput(input)
+
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession.addOutput(captureMetadataOutput)
+
+            captureMetadataOutput.setMetadataObjectsDelegate(delegate, queue: DispatchQueue.main)
+            captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+
+            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            previewLayer.frame = view.layer.bounds
+            previewLayer.videoGravity = .resizeAspectFill
+            view.layer.addSublayer(previewLayer)
+
+            captureSession.startRunning()
+        } catch {
+            print(error)
+            return
+        }
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        captureSession.stopRunning()
+    }
+}
--- QRCode/ViewController.swift
+++ QRCode/ViewController.swift
@@ -6,14 +6,89 @@
 //
 
 import UIKit
+import CoreImage
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var QRCodeImage: UIImageView!
+    @IBOutlet weak var code: UILabel!
+    
+    @IBAction func generateCode(_ sender: UIButton) {
+        let randomNumber = Int.random(in: 100000...999999)
+        code.text = "\(randomNumber)"
+        let data = "\(randomNumber)".data(using: .ascii)
+        
+        guard let filter = CIFilter(name: "CIQRCodeGenerator") else { return }
+        filter.setValue(data, forKey: "inputMessage")
+        
+        guard let ciImage = filter.outputImage else { return }
+        let transform = CGAffineTransform(scaleX: 10, y: 10)
+        
+        let scaleImage = ciImage.transformed(by: transform)
+        QRCodeImage.image = UIImage(ciImage: scaleImage)
     }
 
-
+    // MARK: Capture
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    
+    @IBOutlet weak var scanResualt: UILabel!
+    
+    @IBAction func scanQRCode(_ sender: Any) {
+        let scannerVC = ScannerViewController()
+        scannerVC.delegate = self
+        present(scannerVC, animated: true)
+    }
 }
 
+
+extension ViewController: AVCaptureMetadataOutputObjectsDelegate {
+    
+    func captureQRCode() {
+        let captureDevice = AVCaptureDevice.default(for: .video)
+
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice!)
+            captureSession = AVCaptureSession()
+            captureSession.addInput(input)
+
+            let captureMetaDataOutput = AVCaptureMetadataOutput()
+            captureSession.addOutput(captureMetaDataOutput)
+
+            captureMetaDataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            captureMetaDataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+
+            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+
+            previewLayer.frame = view.layer.bounds
+            previewLayer.videoGravity = .resizeAspectFill
+            view.layer.addSublayer(previewLayer)
+
+            captureSession.startRunning()
+        } catch {
+            print(error)
+            return
+        }
+    }
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let stringValue = readableObject.stringValue else { return }
+
+            DispatchQueue.main.async {
+                self.scanResualt.text = stringValue
+            }
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
+    }
+
+}
